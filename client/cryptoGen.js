c922b688f5dcb0035e6eb0cc9480ff58498e928b
P3ND4 <josecarlospendasrodriguez@gmail.com>
2025-02-17 13:55:30 -0500

    gneratin configs

--- client/cryptoGen.js
+++ client/cryptoGen.js
@@ -7,7 +7,8 @@ const { userInfo } = require('os');
 
 const axios = require("axios");
 const { name } = require('ejs');
-const { createMSPStructureOrg, createMSPStructurePeer}  = require('./utils')
+const { createMSPStructureOrg, createMSPStructurePeer, generateCongigTX } = require('./utils');
+const { domainToUnicode } = require('url');
 
 
 
@@ -17,81 +18,99 @@ const { createMSPStructureOrg, createMSPStructurePeer}  = require('./utils')
 
 
 
-
-
-const GenerateMSP = () => {
+const GenerateMSP = async () => {
   const configPath = path.join(__dirname, '../crypto-config.yaml');
 
-
-  // Crear directorio de configuracion
+  // Leer el archivo de configuración
   let config;
   try {
     const configContent = fs.readFileSync(configPath);
     config = yaml.load(configContent);
-  }
-  catch (err) {
+  } catch (err) {
     console.log('crypto-config.yaml required', err);
     return false;
   }
+
   const network = config.Network || [];
-  let mainPath;
-  mainPath = path.join(__dirname, `../${network['-Name']}-network`);
-  try{
-    fs.rmdirSync(mainPath, {recursive: true})
-  }
-  catch{
-    
-  }
+  let mainPath = path.join(__dirname, `../${network['-Name']}-network`);
+
+  try {
+    fs.rmdirSync(mainPath, { recursive: true });
+  } catch (err) {}
 
   try {
     fs.mkdirSync(mainPath);
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
   }
 
   var orgDirList = {};
-
+  var orgsInfo = [];
+  var orderersIfo = [];
 
   const ordererOrgs = config.OrdererOrgs || [];
+
   // Acceder a las organizaciones de orderers
-  ordererOrgs.forEach(async org => {
+  for (const org of ordererOrgs) {
     console.log('Orderer Org:');
     console.log('  Name:', org.Name);
     console.log('  Domain:', org.Domain);
     orgDirList[org.Domain] = path.join(mainPath, `crypto-config/ordererOrganizations/${org.Domain}`);
-    fs.mkdirSync(orgDirList[org.Domain], { recursive: true })
-    orderersPath = path.join(orgDirList[org.Domain], `orderers/orderer.${org.Domain}`)
-    await createMSPStructurePeer(orderersPath, `orderer.${org.Domain}`, org.Domain, 'orderer')
-    userPath = path.join(orgDirList[org.Domain], `users/Admin@${org.Domain}`)
+    
+    fs.mkdirSync(orgDirList[org.Domain], { recursive: true });
+
+    const orderersPath = path.join(orgDirList[org.Domain], `orderers/orderer.${org.Domain}`);
+    await createMSPStructurePeer(orderersPath, `orderer.${org.Domain}`, org.Domain, 'orderer');
+
+    const userPath = path.join(orgDirList[org.Domain], `users/Admin@${org.Domain}`);
     await createMSPStructureOrg(orgDirList[org.Domain], org.Domain);
     await createMSPStructurePeer(userPath, `Admin@${org.Domain}`, org.Domain, 'admin');
-    fs.mkdirSync(path.join(userPath, 'tls'), { recursive: true })
 
+    fs.mkdirSync(path.join(userPath, 'tls'), { recursive: true });
+    fs.mkdirSync(path.join(orderersPath, 'tls'), { recursive: true });
 
-    fs.mkdirSync(path.join(orderersPath, 'tls'), { recursive: true })
-  
-  });
+    orderersIfo.push({
+      name: org.Name,
+      domain: org.Domain,
+    });
+  }
 
   const peerOrgs = config.PeerOrgs || [];
-  peerOrgs.forEach(async org => {
+
+  // Acceder a las organizaciones de peers
+  for (const org of peerOrgs) {
     console.log('Peer Org:');
     console.log('  Name:', org.Name);
     console.log('  Domain:', org.Domain);
-    orgDirList[org.Domain] = path.join(mainPath, `crypto-config/peerOrganizations/${org.Domain}`);
-    const pathpeers = path.join(orgDirList[org.Domain], 'peers')
-    fs.mkdirSync(orgDirList[org.Domain], { recursive: true })
-    fs.mkdirSync(orgDirList[org.Domain], { recursive: true })
-    await createMSPStructureOrg(orgDirList[org.Domain], org.Domain)
-    const peers = org.Peers || [];
-    peers.forEach(async peer => {
-      const peerPath = path.join(pathpeers, `${peer.Domain}`)
-      console.log('peers:')
-      console.log('Name', peer.Name)
-      console.log('  Domain:', peer.Domain);
-      await createMSPStructurePeer(peerPath, peer.Domain, org.Domain, 'peer')
-    });
-  });
-}
 
-GenerateMSP()
+    orgDirList[org.Domain] = path.join(mainPath, `crypto-config/peerOrganizations/${org.Domain}`);
+    var anchor = null;
+
+    const pathpeers = path.join(orgDirList[org.Domain], 'peers');
+    fs.mkdirSync(orgDirList[org.Domain], { recursive: true });
+
+    await createMSPStructureOrg(orgDirList[org.Domain], org.Domain);
+
+    const peers = org.Peers || [];
+    for (const peer of peers) {
+      const peerPath = path.join(pathpeers, `${peer.Domain}`);
+      console.log('peers:');
+      console.log('Name', peer.Name);
+      console.log('  Domain:', peer.Domain);
+      anchor = peer.Domain;
+      await createMSPStructurePeer(peerPath, peer.Domain, org.Domain, 'peer');
+    }
+
+    orgsInfo.push({
+      name: org.Name,
+      domain: org.Domain,
+      anchor: anchor,
+    });
+  }
+
+  // Esperar a que se complete la generación del archivo de configuración
+  var configTxB = generateCongigTX(orgsInfo, orderersIfo[0].name, orderersIfo[0].domain);
+  fs.writeFileSync(path.join(mainPath, 'configtx.yaml'), configTxB)
+};
+
+GenerateMSP();
--- client/utils.js
+++ client/utils.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-const {execSync} = require('child_process');
-const path  = require('path')
-const axios = require('axios')
+const { execSync } = require('child_process');
+const path = require('path')
+const axios = require('axios');
+const { channel } = require('diagnostics_channel');
 
 const congifYaml = `NodeOUs:
 Enable: true
@@ -18,184 +19,254 @@ OrdererOUIdentifier:
   Certificate: cacerts/my.org-ca-cert.pem
   OrganizationalUnitIdentifier: orderer
 `;
-
-function generateCongigTX(){
-  var port = 7050;
-  const ordererOrgs = (name, domain, port) => {
-    return `        - &${name}Org
-    Name: ${name}Org
-    ID: ${name}MSP
-    MSPDir: crypto-config/ordererOrganizations/${domain}/msp
-    Policies:
-        Readers:
-            Type: Signature
-            Rule: "OR('${name}MSP.member')"
-        Writers:
-            Type: Signature
-            Rule: "OR('${name}MSP.member')"
-        Admins:
-            Type: Signature
-            Rule: "OR('${name}MSP.admin')"
-    OrdererEndpoints:
-        - orderer.acme.com: 7050`
-  }
-  const Orgs = (name, domain, anchor, port) => {
-    return `        - &${name}
-    Name: ${name}MSP
-    ID: ${name}MSP
-    MSPDir: crypto-config/peerOrganizations/${domain}/msp
-    Policies:
-        Readers:
-            Type: Signature
-            Rule: "OR('${name}MSP.admin', '${name}MSP.peer', '${name}MSP.client')"
-        Writers:
-            Type: Signature
-            Rule: "OR('${name}MSP.admin', '${name}MSP.client')"
-        Admins:
-            Type: Signature
-            Rule: "OR('O${name}MSP.admin')"
-        Endorsement:
-            Type: Signature
-            Rule: "OR('${name}MSP.peer')"
-    AnchorPeers:
-        - Host: ${anchor}
-          Port: 7051`
-  }
-  const soloConsensure = (name, domain) =>{
-    return `    Orderer: &OrdererDefaults
-
-    OrdererType: solo
-
-#    OrdererType: etcdraft
-
-    EtcdRaft:
-        Consenters:
-        - Host: ${domain}
-          Port: 7050
-          ClientTLSCert: ../organizations/ordererOrganizations/${domain}/orderers/orderer.${domain}/tls/server.crt
-          ServerTLSCert: ../organizations/ordererOrganizations/${domain}/orderers/orderer.${domain}/tls/server.crt
-
-    Addresses:
-        - orderer.${domain}:7050
-    BatchTimeout: 2s
-    BatchSize:
-        MaxMessageCount: 10
-        AbsoluteMaxBytes: 99 MB
-        PreferredMaxBytes: 512 KB
-
-    Kafka:
-        Brokers:
-            - 127.0.0.1:9092
-    Organizations:
-
-    Policies:
-        Readers:
-            Type: ImplicitMeta
-            Rule: "ANY Readers"
-        Writers:
-            Type: ImplicitMeta
-            Rule: "ANY Writers"
-        Admins:
-            Type: ImplicitMeta
-            Rule: "MAJORITY Admins"
-
-        BlockValidation:
-            Type: ImplicitMeta
-            Rule: "ANY Writers"`
-  }
-  const chanelDefaults = () => {
-    return `    Channel: &ChannelDefaults
-    # Policies defines the set of policies at this level of the config tree
-    # For Channel policies, their canonical path is
-    #   /Channel/<PolicyName>
-    Policies:
-        # Who may invoke the 'Deliver' API
-        Readers:
-            Type: ImplicitMeta
-            Rule: "ANY Readers"
-        # Who may invoke the 'Broadcast' API
-        Writers:
-            Type: ImplicitMeta
-            Rule: "ANY Writers"
-        # By default, who may modify elements at this config level
-        Admins:
-            Type: ImplicitMeta
-            Rule: "MAJORITY Admins"
-
-    # Capabilities describes the channel level capabilities, see the
-    # dedicated Capabilities section elsewhere in this file for a full
-    # description
+const capt = `
     Capabilities:
-        <<: *ChannelCapabilities
+        Channel: &ChannelCapabilities
+            V2_0: true
+    
+        Orderer: &OrdererCapabilities
+            V2_0: true
+    
+        Application: &ApplicationCapabilities
+            V2_0: true      `
+const app = `
+    Application: &ApplicationDefaults
+    
+        Organizations:
+
+        Policies:
+            Readers:
+                Type: ImplicitMeta
+                Rule: "ANY Readers"
+            Writers:
+                Type: ImplicitMeta
+                Rule: "ANY Writers"
+            Admins:
+                Type: ImplicitMeta
+                Rule: "MAJORITY Admins"
+            LifecycleEndorsement:
+                Type: ImplicitMeta
+                Rule: "MAJORITY Endorsement"
+            Endorsement:
+                Type: ImplicitMeta
+                Rule: "MAJORITY Endorsement"
+
+        Capabilities:
+            <<: *ApplicationCapabilities   `
+const ordererOrgs = (name, domain, port) => {
+  return `
+        - &${name}Org
+            Name: ${name}Org
+            ID: ${name}MSP
+            MSPDir: crypto-config/ordererOrganizations/${domain}/msp
+            Policies:
+                Readers:
+                    Type: Signature
+                    Rule: "OR('${name}MSP.member')"
+                Writers:
+                    Type: Signature
+                    Rule: "OR('${name}MSP.member')"
+                Admins:
+                    Type: Signature
+                    Rule: "OR('${name}MSP.admin')"
+            OrdererEndpoints:
+                - orderer.acme.com: 7050`
+}
+const Orgs = (name, domain, anchor, port) => {
+  return `
+        - &${name}
+            Name: ${name}MSP
+            ID: ${name}MSP
+            MSPDir: crypto-config/peerOrganizations/${domain}/msp
+            Policies:
+                Readers:
+                    Type: Signature
+                    Rule: "OR('${name}MSP.admin', '${name}MSP.peer', '${name}MSP.client')"
+                Writers:
+                    Type: Signature
+                    Rule: "OR('${name}MSP.admin', '${name}MSP.client')"
+                Admins:
+                    Type: Signature
+                    Rule: "OR('O${name}MSP.admin')"
+                Endorsement:
+                    Type: Signature
+                    Rule: "OR('${name}MSP.peer')"
+            AnchorPeers:
+                - Host: ${anchor}
+                  Port: 7051`
+}
+const soloConsensure = (name, domain) => {
+  return `    
+    Orderer: &OrdererDefaults
+
+        OrdererType: solo
+
+#       OrdererType: etcdraft
+
+        EtcdRaft:
+            Consenters:
+            - Host: ${domain}
+              Port: 7050
+              ClientTLSCert: ../organizations/ordererOrganizations/${domain}/orderers/orderer.${domain}/tls/server.crt
+              ServerTLSCert: ../organizations/ordererOrganizations/${domain}/orderers/orderer.${domain}/tls/server.crt
+
+        Addresses:
+            - orderer.${domain}:7050
+        BatchTimeout: 2s
+        BatchSize:
+            MaxMessageCount: 10
+            AbsoluteMaxBytes: 99 MB
+            PreferredMaxBytes: 512 KB
+
+        Kafka:
+            Brokers:
+                - 127.0.0.1:9092
+        Organizations:
+
+        Policies:
+            Readers:
+                Type: ImplicitMeta
+                Rule: "ANY Readers"
+            Writers:
+                Type: ImplicitMeta
+                Rule: "ANY Writers"
+            Admins:
+                Type: ImplicitMeta
+                Rule: "MAJORITY Admins"
+
+            BlockValidation:
+                Type: ImplicitMeta
+                Rule: "ANY Writers"`
+}
+const orgNamesC = (orgs) => {
+  result = ''
+  orgs.forEach(org => {
+    result += `                    - *${org.name}\n`
+  })
+  return result
+}
+const orgNamesA = (orgs) => {
+  result = ''
+  orgs.forEach(org => {
+    result += `                - *${org.name}\n`
+  })
+  return result
+}
+
+const channelDefaults = (orgs) => {
+  return `
+    Channel: &ChannelDefaults
+        # Policies defines the set of policies at this level of the config tree
+        # For Channel policies, their canonical path is
+        #   /Channel/<PolicyName>
+          Policies:
+              # Who may invoke the 'Deliver' API
+              Readers:
+                  Type: ImplicitMeta
+                  Rule: "ANY Readers"
+              # Who may invoke the 'Broadcast' API
+              Writers:
+                  Type: ImplicitMeta
+                  Rule: "ANY Writers"
+              # By default, who may modify elements at this config level
+              Admins:
+                  Type: ImplicitMeta
+                  Rule: "MAJORITY Admins"
+
+        # Capabilities describes the channel level capabilities, see the
+        # dedicated Capabilities section elsewhere in this file for a full
+        # description
+          Capabilities:
+              <<: *ChannelCapabilities
 
 ################################################################################
 #   Profile
 ################################################################################
-Profiles:
+    Profiles:
 
-    ThreeOrgsOrdererGenesis:
-        <<: *ChannelDefaults
-        Orderer:
-            <<: *OrdererDefaults
-            Organizations:
-                - *OrdererOrg
-            Capabilities:
-                <<: *OrdererCapabilities
-        Consortiums:
-            SampleConsortium:
+        MultiOrgsOrdererGenesis:
+            <<: *ChannelDefaults
+            Orderer:
+                <<: *OrdererDefaults
                 Organizations:
-                    - *Org1
-                    - *Org2
-                    - *Org3
-    ThreeOrgsChannel:
-        Consortium: SampleConsortium
-        <<: *ChannelDefaults
-        Application:
-            <<: *ApplicationDefaults
-            Organizations:
-                - *Org1
-                - *Org2
-                - *Org3
+                    - *OrdererOrg
+                Capabilities:
+                    <<: *OrdererCapabilities
+            Consortiums:
+                SampleConsortium:
+                    Organizations:
+${orgNamesC(orgs)}
+        MultiOrgsChannel:
+            Consortium: SampleConsortium
+            <<: *ChannelDefaults
+            Application:
+                <<: *ApplicationDefaults
+                Organizations:
+${orgNamesA(orgs)}
             Capabilities:
                 <<: *ApplicationCapabilities`
-  }
-} 
+}
+
+
+
+
+
+
+
+
+
+
+function generateCongigTX(orgs, OrderersName, OrderersDomain) {
+  var port = 7050;
+  result ='---\n' + '    Organizations:\n'+ ordererOrgs(OrderersName, OrderersDomain, port) + '\n'
+  orgsBody = ''
+  orgs.forEach(org => {
+    orgsBody += Orgs(org.name, org.domain, org.anchor, port+1) + '\n'
+  })
+  result += orgsBody + capt + '\n' + app + '\n' + soloConsensure(OrderersName, OrderersDomain) +'\n' + channelDefaults(orgs) + '\n'
+
+
+  return result
+
+}
+
+
 
 
 function generateECDSACSR(commonName, organization, ou, dir_k = __dirname) {
-    const keyPath = path.join(dir_k, `priv_sk`);
-    const csrPath = path.join(dir_k, `${commonName}.csr`);
-  
-    try {
-        // 1️⃣ Generar clave privada ECDSA (prime256v1)
-        execSync(`openssl ecparam -name prime256v1 -genkey -noout -out ${keyPath}`);
-  
-        // 2️⃣ Crear el CSR
-        execSync(`openssl req -new -key ${keyPath} -out ${csrPath} -subj "/CN=${commonName}/O=${organization}/OU=${ou}"`);
-        // Leer los archivos generados
-        const privateKey = fs.readFileSync(keyPath, "utf8");
-        const csr = fs.readFileSync(csrPath, "utf8");
-        fs.rmSync(csrPath)
-  
-        return { key: privateKey, csr };
-    } catch (error) {
-        console.error("❌ Error generando CSR:", error.message);
-        throw error;
-    }
+  const keyPath = path.join(dir_k, `priv_sk`);
+  const csrPath = path.join(dir_k, `${commonName}.csr`);
+
+  try {
+    // 1️⃣ Generar clave privada ECDSA (prime256v1)
+    execSync(`openssl ecparam -name prime256v1 -genkey -noout -out ${keyPath}`);
+
+    // 2️⃣ Crear el CSR
+    execSync(`openssl req -new -key ${keyPath} -out ${csrPath} -subj "/CN=${commonName}/O=${organization}/OU=${ou}"`);
+    // Leer los archivos generados
+    const privateKey = fs.readFileSync(keyPath, "utf8");
+    const csr = fs.readFileSync(csrPath, "utf8");
+    fs.rmSync(csrPath)
+
+    return { key: privateKey, csr };
+  } catch (error) {
+    console.error("❌ Error generando CSR:", error.message);
+    throw error;
   }
+}
 
 
 
 async function signCsr(mspPath, domain, org, ou) {
   const keyPath = path.join(mspPath, 'keystore');
   const certPath = path.join(mspPath, 'signcerts');
-  const {key, csr } = generateECDSACSR(domain, org, ou, keyPath);
+  const { key, csr } = generateECDSACSR(domain, org, ou, keyPath);
   const response = await axios.post("http://localhost:2000/sign-csr", {
     domain,
     csr
   });
   fs.writeFileSync(path.join(certPath, `${domain}-cert.pem`), response.data.cert, 'utf8');
-  return {key: key , cert: response.data.cert }
+  return { key: key, cert: response.data.cert }
 }
 
 
@@ -210,7 +281,7 @@ async function createMSPStructurePeer(baseDir, domain, org, ou) {
     const fullPath = path.join(mspBase, dirName);
     fs.mkdirSync(fullPath, { recursive: true });
     console.log(`Directorio creado: ${fullPath}`);
-    if (dirName in ['cacerts', 'tlscacerts']){
+    if (dirName in ['cacerts', 'tlscacerts']) {
       fs.writeFileSync(path.join(fullPath, 'my.org-ca-cert.pem'), response.data.cert);
     }
   });
@@ -220,7 +291,7 @@ async function createMSPStructurePeer(baseDir, domain, org, ou) {
   fs.writeFileSync(path.join(tls_dir, 'server.crt'), sign['cert'])
   fs.writeFileSync(path.join(tls_dir, 'server.key'), sign['key'])
 }
-async function createMSPStructureOrg(baseDir, domain){
+async function createMSPStructureOrg(baseDir, domain) {
   const mspDirs = ['cacerts', 'tlscacerts']
   const mspBase = path.join(baseDir, 'msp');
   const response = await axios.get("http://localhost:2000/root-ca");
@@ -229,10 +300,10 @@ async function createMSPStructureOrg(baseDir, domain){
     fs.mkdirSync(fullPath, { recursive: true });
     fs.writeFileSync(path.join(fullPath, 'my.org-ca-cert.pem'), response.data.cert);
     console.log(`Directorio creado: ${fullPath}`);
-    
+
   });
   fs.writeFileSync(path.join(mspBase, 'config.yaml'), congifYaml)
-  
+
 
 }
 
@@ -240,4 +311,4 @@ async function createMSPStructureOrg(baseDir, domain){
 
 
 
-module.exports =  {generateECDSACSR, createMSPStructureOrg, createMSPStructurePeer}
+module.exports = { generateECDSACSR, createMSPStructureOrg, createMSPStructurePeer, generateCongigTX }
